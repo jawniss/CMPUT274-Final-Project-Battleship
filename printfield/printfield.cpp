1ca7023e0e70d1f872470f131891485ca073a89a
cmput274 <cmput274@ualberta.ca>
2018-11-25 23:25:07 -0700

    nicer looking ui

--- printfield/printfield.cpp
+++ printfield/printfield.cpp
@@ -2,10 +2,10 @@
 
 const int Pin1 = 1;
 void setup() {
-	// setup function for initializing
-	init();
-	Serial.begin(9600);
-	Serial3.begin(9600);
+    // setup function for initializing
+    init();
+    Serial.begin(9600);
+    Serial3.begin(9600);
 }
 
 
@@ -18,7 +18,7 @@ int e1 = 0, e2 = 0, e3 = 0, e4 = 0, e5 = 0;
 /*
 COULD TRY
 char a1 = X
-	maybe before error was char a1 = "x"
+    maybe before error was char a1 = "x"
   not valid because strings are different from chars?
 */
 
@@ -26,45 +26,69 @@ char a1 = X
 /*
 int positionOfShip;
 void battlefield(){
-	// THE SERIAL PRINT FUNCTION DOESN'T ALLOW MULTIPLE VARIABLES TO BE PRINTED
+    // THE SERIAL PRINT FUNCTION DOESN'T ALLOW MULTIPLE VARIABLES TO BE PRINTED
   Serial.println(a1, a2, a3, a4, a5);
   Serial.println(b1, b2, b3, b4, b5);
   Serial.println(c1, c2, c3, c4, c5);
-	Serial.println(d1, d2, d3, d4, d5);
-	Serial.println(e1, e2, e3, e4, e5);
-	}
+    Serial.println(d1, d2, d3, d4, d5);
+    Serial.println(e1, e2, e3, e4, e5);
+    }
+*/
+/*
+void battlefield() {
+    Serial.print(a1);
+    Serial.print(a2);
+    Serial.print(a3);
+    Serial.print(a4);
+    Serial.println(a5);
+    Serial.print(b1);
+    Serial.print(b2);
+    Serial.print(b3);
+    Serial.print(b4);
+    Serial.println(b5);
+    Serial.print(c1);
+    Serial.print(c2);
+    Serial.print(c3);
+    Serial.print(c4);
+    Serial.println(c5);
+    Serial.print(d1);
+    Serial.print(d2);
+    Serial.print(d3);
+    Serial.print(d4);
+    Serial.println(d5);
+    Serial.print(e1);
+    Serial.print(e2);
+    Serial.print(e3);
+    Serial.print(e4);
+    Serial.println(e5);
+}
 */
 
 void battlefield() {
-	Serial.print(a1);
-	Serial.print(a2);
-	Serial.print(a3);
-	Serial.print(a4);
-	Serial.println(a5);
-	Serial.print(b1);
-	Serial.print(b2);
-	Serial.print(b3);
-	Serial.print(b4);
-	Serial.println(b5);
-	Serial.print(c1);
-	Serial.print(c2);
-	Serial.print(c3);
-	Serial.print(c4);
-	Serial.println(c5);
-	Serial.print(d1);
-	Serial.print(d2);
-	Serial.print(d3);
-	Serial.print(d4);
-	Serial.println(d5);
-	Serial.print(e1);
-	Serial.print(e2);
-	Serial.print(e3);
-	Serial.print(e4);
-	Serial.println(e5);
+  Serial.println(  "Positioning: \n");
+    Serial.println(  "  "  "0"  " |  "  "1"  " |  "  "2"  " | "  "3"  "  | "  "4" );
+    Serial.println(  "-----------------------");
+    Serial.println(  "  "  "5"  " |  "  "6"  " |  "  "7"  " | "  "8"  "  | "  "9"  );
+    Serial.println(  "-----------------------");
+    Serial.println(  " "  "10"  " | "  "11"  " | "  "12"  " | "  "13"  " | "  "14" );
+    Serial.println(  "-----------------------");
+    Serial.println(  " "  "15"  " | "  "16"  " | "  "17"  " | "  "18"  " | "  "19" );
+    Serial.println(  "-----------------------");
+    Serial.println(  " "  "20"  " | "  "21"  " | "  "22"  " | "  "23"  " | "  "24" );
+
+  /*
+    Serial.print(  "\n\n"  "Your opponents field: "   counter (placeholder)  " their ships left!\n";
+    Serial.print(  " "  position[0]  "  | "  position[1]  "  | "  position[2]  "  | "  position[3]  "  | "  position[4]  );
+    Serial.print(  "-----------------------");
+    Serial.print(  " "  position[5]  "  | "  position[6]  "  | "  position[7]  "  | "  position[8]  "  | "  position[9]  );
+    Serial.print(  "-----------------------");
+    Serial.print(  " "  position[10]  "  | "  position[11]  "  | "  position[12]  "  | "  position[13]  "  | "  position[14]  );
+    Serial.print(  "-----------------------");
+    Serial.print(  " "  position[15]  "  | "  position[16]  "  | "  position[17]  "  | "  position[18]  "  | "  position[19]  );
+    Serial.print(  "-----------------------");
+    Serial.print(  " "  position[20]  "  | "  position[21]  "  | "  position[22]  "  | "  position[23]  "  | "  position[24]  );
+  */
 }
-
-
-
 /*
 void a1(){
   if (a1 == positionOfShip){
@@ -335,9 +359,10 @@ void e5(){
 
 int main() {
     setup();
-		battlefield();
-		//BELOW TRYING TO EFFICIENCY
-		//n = sprintf (buffer, %d %d %d %d %d, a1, a2, a3, a4, a5);
-		//printf ("",buffer,n);
-		return 0;
-	}
+        battlefield();
+        //BELOW TRYING TO EFFICIENCY
+        //n = sprintf (buffer, %d %d %d %d %d, a1, a2, a3, a4, a5);
+        //printf ("",buffer,n);
+    Serial.flush();
+        return 0;
+    }
